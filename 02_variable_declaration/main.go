1dba9723136dcaec9da137fb010de90c866d809b
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-12-03 03:11:08 +0100

    Added variable_declaration.

---
+++ 02_variable_declaration/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var firstName, lastName string = "Barış", "Kılıç"
+
+	// var age int = 40
+	// age = 26
+
+	// var isMarried bool
+	// isMarried = false
+
+	firstName := "Barış"
+	lastName := "Kılıç"
+	age := 26
+	isMarried := false
+
+	fmt.Println("Name:", firstName, lastName)
+	fmt.Println("Age:", age)
+	fmt.Println("Is Married?", isMarried)
+}
